8b25639ba1e5cc677d882e60e3f61c297ffe2091
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-30 19:18:22 +0200

    slider trial

--- src/components/landing.js
+++ src/components/landing.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./footer";
 import Navbar from "./navbar";
+import Slider from "./slider";
 
 class landing extends Component {
   render() {
     return (
       <div>
         <Navbar></Navbar>
+        <Slider></Slider>
         <Footer></Footer>
       </div>
     );
