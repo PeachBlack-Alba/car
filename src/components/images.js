96cce807f2bea78fe5057c04de32684f183e551e
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-05-04 19:26:34 +0200

    slider

--- src/components/images.js
+++ src/components/images.js
@@ -1,28 +1,30 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import Slider from "react-slick";
-
 import image1 from "../images/image1.png";
 import image2 from "../images/image2.png";
 import image3 from "../images/image3.png";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default class images extends Component {
   render() {
     return (
       <div className="wrapper">
-        <div className="page">
-          <Slider
-            speed={500}
-            slidesToShow={1}
-            slidesToScroll={1}
-            infinite={false}
-            dots={true}
-          >
-            <img className="image" src={image1} alt="image1" />
-            <img className="image" src={image2} alt="image2" />
-            <img className="image" src={image3} alt="image3" />
-          </Slider>
-        </div>
+        <Slider
+          speed={500}
+          slidesToShow={1}
+          slidesToScroll={1}
+          infinite={true}
+          arrows={true}
+          focusOnSelect={true}
+        >
+          <img className="image" src={image1} alt="image1" />
+
+          <img className="image" src={image2} alt="image2" />
+
+          <img className="image" src={image3} alt="image3" />
+        </Slider>
       </div>
     );
   }
