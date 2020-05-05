15588731444a664ef61c7124f406293ae674ac02
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-05-05 12:48:47 +0200

    commit

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./footer";
 import Navbar from "./navbar";
 import Images from "./images";
+import Services from "./services";
 
 class landing extends Component {
   render() {
     return (
       <div>
+        <Services></Services>
         <Navbar></Navbar>
         <Images></Images>
         <Footer></Footer>
---
+++ src/components/services.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function services() {
+  return (
+    <div>
+      <p>So kaufen Sie Ihr Fahrzeug mit der Bank 11</p>
+      <div className="containerServices"></div>
+    </div>
+  );
+}
